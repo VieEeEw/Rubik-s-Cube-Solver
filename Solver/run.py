e6e9e4e7cf757736ca411ab341e0c4836cb1dd70
786851669@qq.com <NjSr9803>
2019-03-23 21:17:18 -0500

    Cube solver roll back to raw

--- Solver/run.py
+++ Solver/run.py
@@ -2,17 +2,7 @@ import solver
 
 
 def solve_cube(situ='DUUBULDBFRBFRRULLLBRDFFFBLURDBFDFDRFRULBLUFDURRBLBDUDL'):
-	cube_solution_raw = solver.solve(situ).strip()
-	cube_solution = str()
-	for pro in cube_solution_raw.split():
-		if len(pro) > 1:
-			i = 0
-			while i < int(pro[1]):
-				cube_solution += pro[0]
-				cube_solution += ' '
-				i += 1
-		else:
-			cube_solution += pro[0]
+	cube_solution = solver.solve(situ).strip()
 	f = open('cube_solution.txt', 'w')
 	f.truncate()
 	f.write(cube_solution)
@@ -23,7 +13,7 @@ def solve_cube(situ='DUUBULDBFRBFRRULLLBRDFFFBLURDBFDFDRFRULBLUFDURRBLBDUDL'):
 if __name__ == '__main__':
 	s = input('Input your cube identification.\n(P.S.:Do not put any quote marks)\nYour input here: ')
 	s = s.upper().strip()
-	# solve_cube()
-	solve_cube(s)
+	solve_cube()
+	# solve_cube(s)
 	fh = open("cube_solution.txt")
 	print(fh.read())
